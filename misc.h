1795ded1536892438f9b0af825167aa5624dca76
Scott Chen <48900010+FUTURETECH6@users.noreply.github.com>
2021-08-30 03:28:16 +0000

    add misc.h

---
+++ misc.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+namespace misc {
+    template <typename T>
+    void printVec(std::vector<T> &vec) {
+        using namespace std;
+        for (auto &i : vec)
+            cout << i << " ";
+        cout << endl;
+    }
+    template <typename T>
+    void printVec2D(std::vector<std::vector<T>> &vec) {
+        using namespace std;
+        for (auto &i : vec) {
+            for (auto &j : i)
+                cout << j << " ";
+            cout << endl;
+        }
+        cout << endl;
+    }
+};  // namespace misc
