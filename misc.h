43af0cfafa38b570d0331842cfd6dc36698e3490
xiyao <futuretech6@foxmail.com>
2022-03-25 16:29:06 +0800

    update misc.h, not using ref vector

--- misc.h
+++ misc.h
@@ -3,14 +3,14 @@
 
 namespace misc {
     template <typename T>
-    void printVec(std::vector<T> &vec) {
+    void printVec(std::vector<T> vec) {
         using namespace std;
         for (auto &i : vec)
             cout << i << " ";
         cout << endl;
     }
     template <typename T>
-    void printVec2D(std::vector<std::vector<T>> &vec) {
+    void printVec2D(std::vector<std::vector<T>> vec) {
         using namespace std;
         for (auto &i : vec) {
             for (auto &j : i)
