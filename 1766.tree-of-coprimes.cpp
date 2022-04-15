41414b91831d412e768437ba633c1f1a946fd1dc
xiyao <futuretech6@foxmail.com>
2022-04-15 14:51:41 +0800

    1766.cpp 优化不明白🤮

--- 1766.tree-of-coprimes.cpp
+++ 1766.tree-of-coprimes.cpp
@@ -78,12 +78,18 @@
 
 #include "misc.h"
 using namespace std;
+#include <chrono>
+
+#define DEBUG
 
 // @lc code=start
 
+#include <algorithm>
 #include <map>
 #include <numeric>
 #include <set>
+#include <unordered_map>
+#include <unordered_set>
 
 struct Node {
     unsigned index;
@@ -94,47 +100,152 @@ struct Node {
     Node(unsigned index, int val) : index(index), val(val), parent(nullptr) {}
 };
 
+template <typename Container>  // we can make this generic for any container [1]
+struct container_hash {
+    std::size_t operator()(Container const &V) const {
+        int hash = V.size();
+        for (auto &i : V) {
+            hash ^= i + 0x9e3779b9 + (hash << 6) + (hash >> 2);
+        }
+        return hash;
+    }
+};
+
+struct edge_hash {
+    std::size_t operator()(vector<int> const &edge) const { return ((size_t)edge[0] << 17) | edge[1]; }
+};
+
+struct pair_hash {
+    template <class T1, class T2>
+    std::size_t operator()(const std::pair<T1, T2> &p) const {
+        auto h1 = std::hash<T1>{}(p.first);
+        auto h2 = std::hash<T2>{}(p.second);
+
+        // Mainly for demonstration purposes, i.e. works but is overly simple
+        // In the real world, use sth. like boost.hash_combine
+        return h1 ^ h2;
+    }
+};
+
 class Solution {
   private:
+    unordered_map<int, unsigned> depthMap = {{0, 0}};
+
     int gcd(int a, int b) {
-        static map<pair<int, int>, int> dict;
-        if (dict.find({min(a, b), max(a, b)}) == dict.end())
-            dict[{min(a, b), max(a, b)}] = b > 0 ? gcd(b, a % b) : a;
-        return dict[{min(a, b), max(a, b)}];
+        // static unordered_map<pair<int, int>, int, pair_hash> dict;
+        // if (dict.count({min(a, b), max(a, b)}) == 0)
+        //     dict[{min(a, b), max(a, b)}] = b > 0 ? gcd(b, a % b) : a;
+        // return dict[{min(a, b), max(a, b)}];
+        return b > 0 ? gcd(b, a % b) : a;
+    }
+
+    bool updateDepth(vector<int> edge) {
+        switch (depthMap.count(edge[0]) << 1 | depthMap.count(edge[1])) {
+            case 0b00: return false;
+            case 0b01: depthMap[edge[0]] = depthMap[edge[1]] + 1; break;
+            case 0b10: depthMap[edge[1]] = depthMap[edge[0]] + 1; break;
+            default: return false;
+        }
+        return true;
     }
 
   public:
     vector<int> getCoprimes(vector<int> &nums, vector<vector<int>> &edges) {
+#ifdef DEBUG
+        using namespace std::chrono;
+        vector<int64_t> time;
+        time.push_back(duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count());
+#endif
         vector<Node *> nodes;
         for (unsigned i = 0; i < nums.size(); i++)
             nodes.push_back(new Node(i, nums[i]));
 
-        set<vector<int>> edges_set = set<vector<int>>(edges.begin(), edges.end());
+        // auto edges_set_tmp = set<vector<int>>(edges.begin(), edges.end());
+
+        // unordered_map<int, unordered_set<int>> connected_graph;
+        // for (auto &edge : edges) {
+        //     if (connected_graph.count(edge[0]))
+        //         connected_graph[edge[0]].insert(edge[1]);
+        //     else
+        //         connected_graph[edge[0]] = {edge[1]};
+        //     if (connected_graph.count(edge[1]))
+        //         connected_graph[edge[1]].insert(edge[0]);
+        //     else
+        //         connected_graph[edge[1]] = {edge[0]};
+        // }
+
+        auto edges_set = unordered_set<vector<int>, edge_hash>(edges.begin(), edges.end());
+
+#ifdef DEBUG
+        time.push_back(duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count());
+        cout << "[*] Time to prepare: " << time.back() - *(time.end() - 2) << endl;
+#endif
+
+        //         while (!edges_set_tmp.empty()) {
+        //             for (auto p = edges_set_tmp.begin(); p != edges_set_tmp.end();)
+        //                 if (updateDepth(*p))
+        //                     edges_set_tmp.erase(p++);
+        //                 else
+        //                     p++;
+        //         }
+
+        // #ifdef DEBUG
+        //         time.push_back(duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count());
+        //         cout << "[*] Time set parent: " << time.back() - *(time.end() - 2) << endl;
+        // #endif
+
+        //         sort(edges.begin(), edges.end(), [this](vector<int> &a, vector<int> &b) { return min(this->depthMap[a[0]], this->depthMap[a[1]]) < min(this->depthMap[b[0]], this->depthMap[b[1]]);
+        //         });
+
+        // #ifdef DEBUG
+        //         time.push_back(duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count());
+        //         cout << "[*] Time sort: " << time.back() - *(time.end() - 2) << endl;
+        // #endif
+
+        //         for (auto p = edges.begin(); p < edges.end();) {
+        //             if ((p->at(0) && p->at(1)) == 0) {  // contains root
+        //                 nodes[p->at(0) | p->at(1)]->parent = nodes[0];
+        //             } else {                                                                             // no root
+        //                 if (nodes[p->at(0)]->parent == nullptr && nodes[p->at(1)]->parent == nullptr) {  // 00
+        //                     p++;
+        //                     continue;
+        //                 } else if (nodes[p->at(0)]->parent != nullptr && nodes[p->at(1)]->parent != nullptr) {  // 11
+        //                     cout << "Theoretically unreachable" << endl;
+        //                 } else if (nodes[p->at(0)]->parent != nullptr)  // 10
+        //                     nodes[p->at(1)]->parent = nodes[p->at(0)];
+        //                 else if (nodes[p->at(1)]->parent != nullptr)  // 01
+        //                     nodes[p->at(0)]->parent = nodes[p->at(1)];
+        //             }
+        //             // edges.erase(p);
+        //             p++;
+        //         }
 
         while (!edges_set.empty()) {
+#ifdef DEBUG
+            cout << "while count; edges_set.size() = " << edges_set.size() << endl;
+#endif
             for (auto p = edges_set.begin(); p != edges_set.end();) {
                 if ((p->at(0) && p->at(1)) == 0) {  // contains root
                     nodes[p->at(0) | p->at(1)]->parent = nodes[0];
-                    edges_set.erase(p++);
-                } else {  // no root
-                    if (nodes[p->at(0)]->parent == nullptr && nodes[p->at(1)]->parent == nullptr || nodes[p->at(0)]->parent != nullptr && nodes[p->at(1)]->parent != nullptr) {
+                } else {                                                                             // no root
+                    if (nodes[p->at(0)]->parent == nullptr && nodes[p->at(1)]->parent == nullptr) {  // 00
                         p++;
                         continue;
-                    } else if (nodes[p->at(0)]->parent != nullptr)
+                    } else if (nodes[p->at(0)]->parent != nullptr && nodes[p->at(1)]->parent != nullptr) {  // 11
+                        cout << "Theoretically unreachable" << endl;
+                    } else if (nodes[p->at(0)]->parent != nullptr)  // 10
                         nodes[p->at(1)]->parent = nodes[p->at(0)];
-                    else if (nodes[p->at(1)]->parent != nullptr)
+                    else if (nodes[p->at(1)]->parent != nullptr)  // 01
                         nodes[p->at(0)]->parent = nodes[p->at(1)];
-
-                    edges_set.erase(p++);
                 }
+                edges_set.erase(p++);
             }
         }
 
-        // for (unsigned i = 0; i < nodes.size(); i++)
-        //     if (nodes[i]->parent)
-        //         cout << i << ": " << nodes[i]->parent->index << endl;
-        //     else
-        //         cout << -1 << endl;
+#ifdef DEBUG
+        time.push_back(duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count());
+        cout << "[*] Time set parent: " << time.back() - *(time.end() - 2) << endl;
+#endif
 
         vector<int> result;
         for (auto &node : nodes) {
@@ -150,6 +261,11 @@ class Solution {
                 result.push_back(-1);
         }
 
+#ifdef DEBUG
+        time.push_back(duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count());
+        cout << "[*] Time find gcd:   " << time.back() - *(time.end() - 2) << endl;
+#endif
+
         return result;
     }
 };
@@ -164,17 +280,20 @@ int main(int argc, char const *argv[]) {
     // vector<vector<int>> edges = {{17, 0}, {30, 17}, {41, 30}, {10, 30}, {13, 10}, {7, 13}, {6, 7}, {45, 10}, {2, 10}, {14, 2}, {40, 14}, {28, 40}, {29, 40}, {8, 29}, {15, 29}, {26, 15}, {23, 40},
     //     {19, 23}, {34, 19}, {18, 23}, {42, 18}, {5, 42}, {32, 5}, {16, 32}, {35, 14}, {25, 35}, {43, 25}, {3, 43}, {36, 25}, {38, 36}, {27, 38}, {24, 36}, {31, 24}, {11, 31}, {39, 24}, {12, 39},
     //     {20, 12}, {22, 12}, {21, 39}, {1, 21}, {33, 1}, {37, 1}, {44, 37}, {9, 44}, {46, 2}, {4, 46}};
-    // [-1,21,17,43,10,42,7,13,29,44,17,31,39,10,10,29,32,0,40,23,12,39,12,40,25,35,15,38,40,40,17,24,5,1,19,14,17,21,25,24,14,17,40,25,37,17,10]
+    // // [-1,21,17,43,10,42,7,13,29,44,17,31,39,10,10,29,32,0,40,23,12,39,12,40,25,35,15,38,40,40,17,24,5,1,19,14,17,21,25,24,14,17,40,25,37,17,10]
 
-    vector<int> nums = {3};     // size = 2e+5 + 1
-    vector<vector<int>> edges;  // size = 2e+5
-    for (unsigned i = 0; i < 1e+5; i++) {
+    auto nums_size   = 1e+5;
+    vector<int> nums = {3};     // size = 1e+5
+    vector<vector<int>> edges;  // size = 1e+5 - 1
+    for (unsigned i = 0; i < (nums_size - 1) / 2; i++) {
         nums.push_back(2);
         nums.push_back(6);
     }
-    for (int i = 1; i <= 2e+5; i++)
+    nums.pop_back();
+    for (int i = 1; i < nums_size; i++)
         edges.push_back({0, i});
 
-    misc::printVec(Solution().getCoprimes(nums, edges));
+    // misc::printVec(Solution().getCoprimes(nums, edges));
+    Solution().getCoprimes(nums, edges);
     return 0;
 }
